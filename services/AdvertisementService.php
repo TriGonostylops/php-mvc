2d49c826c91e7449fae41b6921e47f9d267c3012
TriGonostylops <marton.balazsi@avolantis.net>
2024-06-24 14:56:48 +0200

    services for advertisement, user

--- services/AdvertisementService.php
+++ services/AdvertisementService.php
@@ -1,1 +1,25 @@
 <?php
+
+require_once 'config/database.php';
+require_once 'models/User.php';
+
+class UserService
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance()->getConnection();
+    }
+
+    public function getAllUsers()
+    {
+        $stmt = $this->db->prepare("SELECT * FROM users");
+        $stmt->execute();
+        $users = [];
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $users[] = new User($row['id'], $row['name']);
+        }
+        return $users;
+    }
+}
--- services/UserService.php
+++ services/UserService.php
@@ -1,1 +1,21 @@
 <?php
+require_once 'config/database.php';
+require_once 'models/Advertisement.php';
+
+class AdvertisementService {
+    private $db;
+
+    public function __construct() {
+        $this->db = Database::getInstance()->getConnection();
+    }
+
+    public function getAllAdvertisements() {
+        $stmt = $this->db->prepare("SELECT advertisements.*, users.name as username FROM advertisements JOIN users ON advertisements.userid = users.id");
+        $stmt->execute();
+        $ads = [];
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $ads[] = ['ad' => new Advertisement($row['id'], $row['userid'], $row['title']), 'username' => $row['username']];
+        }
+        return $ads;
+    }
+}
