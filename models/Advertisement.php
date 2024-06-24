7b7730406bf177afcf4895d726972f67f0a7b295
TriGonostylops <marton.balazsi@avolantis.net>
2024-06-24 14:34:40 +0200

    models for advertisement, user

--- models/Advertisement.php
+++ models/Advertisement.php
@@ -1,1 +1,12 @@
 <?php
+class Advertisement {
+    public $id;
+    public $userid;
+    public $title;
+
+    public function __construct($id, $userid, $title) {
+        $this->id = $id;
+        $this->userid = $userid;
+        $this->title = $title;
+    }
+}
--- models/User.php
+++ models/User.php
@@ -1,1 +1,10 @@
 <?php
+class User {
+    public $id;
+    public $name;
+
+    public function __construct($id, $name) {
+        $this->id = $id;
+        $this->name = $name;
+    }
+}
